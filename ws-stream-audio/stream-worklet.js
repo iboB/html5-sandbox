2ce9fa01135a060ed489b88aa3ecdff4f7a44c33
Borislav Stanimirov <b.stanimirov@abv.bg>
2024-01-17 09:54:15 +0200

    playing audio

--- ws-stream-audio/stream-worklet.js
+++ ws-stream-audio/stream-worklet.js
@@ -1,11 +1,30 @@
 class StreamWorklet extends AudioWorkletProcessor {
+  constructor() {
+    super();
+    this.port.onmessage = this.onmessage.bind(this)
+  }
+
+  onmessage(msg) {
+    const { data } = msg;
+    this.buf = new Float32Array(data);
+    this.bi = 0;
+  }
+
   process(inputs, outputs) {
-    const output = outputs[0];
-    output.forEach((channel) => {
+    const channel = outputs[0][0];
+    if (!this.buf) {
       for (let i = 0; i < channel.length; i++) {
         channel[i] = Math.random() * 2 - 1;
       }
-    });
+    }
+    else {
+      for (let i = 0; i < channel.length; i++) {
+        channel[i] = this.buf[this.bi++]
+        if (this.bi == this.buf.length) {
+          this.bi = 0;
+        }
+      }
+    }
     return true;
   }
 }
