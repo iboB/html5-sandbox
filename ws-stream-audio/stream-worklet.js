87a46164220295bbfbfab7fbe9e581580bdee58f
Borislav Stanimirov <b.stanimirov@abv.bg>
2024-01-17 05:18:38 +0200

    ws-stream-audio: wip, initial noise example

---
+++ ws-stream-audio/stream-worklet.js
@@ -0,0 +1,13 @@
+class StreamWorklet extends AudioWorkletProcessor {
+  process(inputs, outputs) {
+    const output = outputs[0];
+    output.forEach((channel) => {
+      for (let i = 0; i < channel.length; i++) {
+        channel[i] = Math.random() * 2 - 1;
+      }
+    });
+    return true;
+  }
+}
+
+registerProcessor('stream-worklet', StreamWorklet);
